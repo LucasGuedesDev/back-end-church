fdb847bea65e03ed8967c88e4c2acf8abfe8dba8
André Lacerda <andredesenvolvedorti@gmail.com>
2023-12-08 09:48:40 -0300

    Implementação de segurança e token

---
+++ src/main/java/com/apichurch/services/SecurityFilter.java
@@ -0,0 +1,46 @@
+package com.apichurch.services;
+
+import com.apichurch.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token !=null){
+            var login = tokenService.validateToken(token);
+            UserDetails user = repository.findByLogin(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var autHeader = request.getHeader("Authorization");
+        if(autHeader == null) return null;
+        return autHeader.replace("Bearer ", "");
+    }
+}
